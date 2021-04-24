24961042dfdb04f9e2f93271c0f8cb4ded3a2bcb
selinoztuurk <selinnur@gmail.com>
2021-04-24 23:32:42 +0300

    linear regression function implemented

--- Homework2/linear_regression.py
+++ Homework2/linear_regression.py
@@ -8,15 +8,23 @@ def linear_regression(dataset):
 
     clear_dataset = dataset.dropna(axis=0)
     clear_dataset = clear_dataset.reset_index(drop=True)
+
     X = clear_dataset.iloc[:, :-1]
     y = clear_dataset[clear_dataset.columns[-1]]
+
     coefficients = ((np.linalg.inv((X.T).dot(X))).dot(X.T).dot(y))
+
     y_estimated = X.dot(coefficients)
+
     error = y - y_estimated
+
     variance = ((error.dot(error))/(len(clear_dataset) - len(X.columns) - 1))*((np.linalg.inv((X.T).dot(X))))
+
     t_stats = stats.t.ppf(1.95/2, len(clear_dataset) - 1)
+
     intervals = []
     for i in range(0, len(coefficients)):
         intervals.append([coefficients[i]-t_stats*variance[i][i], coefficients[i]+t_stats*variance[i][i]])
+
     return coefficients, error, intervals
 
